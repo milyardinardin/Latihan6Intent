6096207bcec1b473e28c85130594813f3ec7153a
milyardinardin <labanamilyardin@gmail.com>
2020-08-24 17:41:30 +0700

    Inten

---
+++ app/src/main/java/com/permanacode/inten/activity_main2.java
@@ -0,0 +1,19 @@
+package com.permanacode.inten;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class activity_main2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        TextView t = (TextView) findViewById(R.id.text_viewdata);
+        Bundle bundle=getIntent().getExtras();
+        String s=bundle.getString("name");
+        t.setText(s);
+
+    }
+}
